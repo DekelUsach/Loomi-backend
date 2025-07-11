2eb7a7b6c443b2c20a3f801e96f3e2da31688e60
Android Studio <devandroid@ort>
2025-07-11 11:51:03 -0300

    Added login method and getProfile method

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-import { getAllUsers, getUserById, createUser } from '../controllers/userController.js';
+import { getAllUsers, getUserById, createUser, loginUser } from '../controllers/userController.js';
+import { getProfile } from '../controllers/profileController.jsx'
 
 const router = Router();
 
 router.get('/', getAllUsers);
 router.get('/:id', getUserById);
 router.post('/', createUser);
+router.post('/login', loginUser); // <--- agregá esto
+router.get('/profile', getProfile);
+
+
 
 export default router;
